5e6973da6fb8beae2bc6361d89e697f7a20a71a5
KavitaJadhav <jadhavkavita206@gmail.com>
2023-11-13 17:45:34 +0530

    Added assertions in the test.

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 import http from 'k6/http';
+import {check} from 'k6';
 
 export default function () {
     let res = http.get('http://test.k6.io');
+    check(res, {
+        'success response': (result) => result.status === 200,
+        'body contains text': (result) => result.body.includes('Collection of simple web-pages suitable for load testing.')
+    })
 };
