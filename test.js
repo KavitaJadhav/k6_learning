eaeb5ad6011e835debed9574f724a6bc0483a663
KavitaJadhav <jadhavkavita206@gmail.com>
2023-11-13 17:44:01 +0530

    Added options in the test file.

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 import http from 'k6/http';
 
+export let options = {
+    vus: 10,
+    iterations: 100,
+    output: 'json=test_result1.json'
+};
+
 export default function () {
     let res = http.get('http://test.k6.io');
 };
-
