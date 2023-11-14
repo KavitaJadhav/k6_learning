5d39a6c3ce61390772cc266159a13c8e522ce2c4
KavitaJadhav <jadhavkavita206@gmail.com>
2023-11-14 09:33:07 +0530

    Added dynamic tag.

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import execution from 'k6/execution';
 import {check} from 'k6';
 
 export default function () {
+    execution.vu.tags.environment = __ENV.ENVIRONMENT;
     console.log(execution.vu.tags)
 
     let res = http.get('http://test.k6.io');
@@ -11,3 +12,6 @@ export default function () {
         'body contains text': (result) => result.body.includes('Collection of simple web-pages suitable for load testing.')
     })
 };
+
+// Command to pass env variable
+// k6 run  test.js --out json=test.json --config load-test-config.json -e ENVIRONMENT='uat'
