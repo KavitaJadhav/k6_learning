f03e1dc19c29fbf6e21c0be12ce7c2bea30ea4c4
KavitaJadhav <jadhavkavita206@gmail.com>
2023-11-14 09:32:05 +0530

    Added tags in the configuration

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 import http from 'k6/http';
+import execution from 'k6/execution';
 import {check} from 'k6';
 
 export default function () {
+    console.log(execution.vu.tags)
+
     let res = http.get('http://test.k6.io');
     check(res, {
         'success response': (result) => result.status === 200,
