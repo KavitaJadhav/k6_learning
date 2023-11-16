1ff3c2d6324edbd3b65b8a26098e5e5b0172fb18
KavitaJadhav <jadhavkavita206@gmail.com>
2023-11-16 13:23:11 +0530

    Integrated prometheous plugin.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM golang:1.20-alpine as builder
 WORKDIR /app
 RUN go install go.k6.io/xk6/cmd/xk6@v0.9.2
-RUN xk6 build --with github.com/grafana/xk6-sql@v0.2.1
+RUN xk6 build --with github.com/grafana/xk6-sql@v0.2.1 --with github.com/grafana/xk6-output-prometheus-remote@v0.2.0
 
 
 FROM alpine
--- commands.sh
+++ commands.sh
@@ -24,3 +24,22 @@ k6 run v1/test.k6.io/test.js \
 --config v1/test.k6.io/load-test-config.json \
 --out json=summary/test.json \
 -e ENVIRONMENT='uat'
+
+#Create volume for prometheus data
+docker volume create prometheus-data
+
+#Run prometheus server locally
+docker run \
+-p 9090:9090 \
+--name=prometheus \
+-v prometheus-data:/prometheus prom/prometheus \
+--web.enable-remote-write-receiver --config.file=/etc/prometheus/prometheus.yml
+
+#run tests with prometheus server variables
+docker run -i -v $(pwd):/home/k6 k6_learning \
+k6 run v1/test.k6.io/test.js \
+--config v1/test.k6.io/load-test-config.json \
+--out json=summary/test.json \
+-e ENVIRONMENT='uat' \
+-e K6_PROMETHEUS_RW_SERVER_URL=http://docker.for.mac.localhost:9090/api/v1/write \
+-o experimental-prometheus-rw
